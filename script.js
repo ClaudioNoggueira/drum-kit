63d614265d90ac808de03cd08a9f01375d9c75f4
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2021-12-10 16:52:47 -0300

    Button click events

---
+++ script.js
@@ -0,0 +1,43 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w": // crash
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "a": // tom 1
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break
+            case "s": // tom 2
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break
+            case "d": // snare
+                var tom2 = new Audio("sounds/snare.mp3");
+                tom2.play();
+                break
+            case "j": // tom 3
+                var tom2 = new Audio("sounds/tom-3.mp3");
+                tom2.play();
+                break
+            case "k": // tom 4
+                var tom2 = new Audio("sounds/tom-4.mp3");
+                tom2.play();
+                break
+            case "l": // kick
+                var tom2 = new Audio("sounds/kick-bass.mp3");
+                tom2.play();
+                break
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+    });
+}
+
+// '() => {}' and 'function () {}' are diferent
+
+
+
